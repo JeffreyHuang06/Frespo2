3bea7065db0dc20d5e29f0c9f4f0e1296cbea219
FeverDreme <jeffreyhuang08@gmail.com>
2021-02-02 01:29:15 -0500

    IMPROVE: alt msgs now

--- src/components/banner/Icon.tsx
+++ src/components/banner/Icon.tsx
@@ -2,14 +2,15 @@ import React from 'react'
 
 interface Props {
     src: string;
+    alt?: string;
 }
 
-const Icon: React.FC<Props> = ({src}) => {
+const Icon: React.FC<Props> = ({src, alt="image"}) => {
     return (
         <div className='Icon'>
-            <img src={src} />
+            <img src={src} alt={alt}/>
         </div>
     );
 }
 
-export default Icon;
+export default Icon; 
