1a5b88deba142fccebce78221885dbe36496b4ba
FeverDreme <jeffreyhuang08@gmail.com>
2021-02-05 04:31:11 -0500

    NEW: About page

---
+++ src/components/About.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './about/About.scss'
+
+export default function About() {
+    return (
+        <div className="About">
+            <pre id="abouttext">
+                Frespo: <br />
+                
+                <br />
+
+                Free Speech, <strong>expression</strong>, <br />
+                <i>(no)</i> bias nor, political <br />
+                affiliation. <br />
+
+                <br />
+
+                Created by Jeffrey Huang
+            </pre>
+        </div>
+    )
+}
