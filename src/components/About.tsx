50c313556aecf27c0bbc01ff3353e1b2ba3802c6
FeverDreme <jeffreyhuang08@gmail.com>
2021-02-07 00:19:02 -0500

    stuff

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -7,15 +7,18 @@ export default function About() {
             <pre id="abouttext">
                 Frespo: <br />
                 
-                <br />
+                {/* <br />
 
                 Free Speech, <strong>expression</strong>, <br />
                 <i>(no)</i> bias nor, political <br />
                 affiliation. <br />
 
-                <br />
+                <br /> */}
 
                 Created by Jeffrey Huang
+
+                <br />
+                <a href="https://github.com/JeffreyHuang06/Frespo2"><button>My GitHub</button></a>
             </pre>
         </div>
     )
