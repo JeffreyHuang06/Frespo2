a8048e8e1bf58a10e78f26a274ab233e964ed904
FeverDreme <jeffreyhuang08@gmail.com>
2021-02-07 00:12:13 -0500

    NEW: img width

--- src/components/banner/Icon.tsx
+++ src/components/banner/Icon.tsx
@@ -4,12 +4,14 @@ interface Props {
     src: string;
     alt?: string;
     title?: string;
+    width?: number;
+    height?: number;
 }
 
-const Icon: React.FC<Props> = ({src, alt="image", title=""}) => {
+const Icon: React.FC<Props> = ({src, alt="image", title="", width, height}) => {
     return (
         <div className='Icon'>
-            <img src={src} alt={alt} title={title} />
+            <img src={src} alt={alt} title={title} width={width} height={height} />
         </div>
     );
 }
