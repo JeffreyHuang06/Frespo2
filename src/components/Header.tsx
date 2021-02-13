ce32c9c8eb78729ff6fe40a81e3d0c89c94c2cb3
FeverDreme <jeffreyhuang08@gmail.com>
2021-02-12 19:35:32 -0500

    REFACTOR: changed header location

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 
-import './Header.scss'
+import './header/Header.scss'
 
 interface Props {
     text: string;
 }
 
 const Header: React.FC<Props> = ({text}) => {
+    if (text === "") return null;
+
     return (
         <div className="Header">
             <h1>{text}</h1>
