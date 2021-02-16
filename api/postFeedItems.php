c1a912d694da30351c469ba41bc8965d350adad8
FeverDreme <jeffreyhuang08@gmail.com>
2021-02-15 20:35:40 -0500

    NEW: post with whom

--- api/postFeedItems.php
+++ api/postFeedItems.php
@@ -5,7 +5,7 @@
     $rest_json = file_get_contents("php://input");
     $_POST = json_decode($rest_json, true);
 
-    function writeSQL($t, $c){
+    function writeSQL($t, $c, $w){
         require './credentials.php';
 
         $conn = new mysqli($servername, $username, $password, $dbname . "feed");
@@ -17,11 +17,12 @@
         // sanitize mailicous code
         $t = nl2br(htmlentities($t, ENT_QUOTES));
         $c = nl2br(htmlentities($c, ENT_QUOTES));
+        $w = nl2br(htmlentities($w, ENT_QUOTES));
         $date = date("Y-m-d H:i:s");
         $hash = hash('sha256', $t . $c . $date);
 
-        $stmt = $conn->prepare("INSERT INTO feed VALUES (?,?,?,?)");
-        $stmt->bind_param("ssss", $t, $c, $date, $hash);
+        $stmt = $conn->prepare("INSERT INTO feed VALUES (?,?,?,?,?)");
+        $stmt->bind_param("sssss", $t, $c, $date, $hash, $w);
         $worked = $stmt->execute();
 
         $conn->close();
@@ -35,8 +36,9 @@
 
         $title = $_POST['title'];
         $content = $_POST['content'];
+        $whom = $_POST['whom'];
 
-        writeSQL($title, $content);
+        writeSQL($title, $content, $whom);
         
     } else {
         echo "error: invalid form input";
--- src/getpost/postFeed.ts
+++ src/getpost/postFeed.ts
@@ -1,13 +1,18 @@
 import axios from 'axios'
 import env from 'react-dotenv'
+import Cookies from 'js-cookie'
 
 const postFeed = async(title: string, content: string): Promise<boolean> => {
+    let whom = Cookies.get("username");
+    if (whom === undefined) whom = "Guest";
+
     const res = await axios({
         method: 'post',
         url: `${env.API_PATH}/postFeedItems.php`,
         data: {
             "title": title,
-            "content": content
+            "content": content,
+            "whom": whom
         }
     });
     console.log(res);
