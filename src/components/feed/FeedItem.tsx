2d0c4e22edbdcfe7f72e6ecb333ef1909799a454
FeverDreme <jeffreyhuang08@gmail.com>
2021-02-16 01:01:09 -0500

    NEW: delete as admin

--- src/components/feed/FeedItem.tsx
+++ src/components/feed/FeedItem.tsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, {useState} from 'react'
 import If from '../If'
 import {checkAdmin} from '../../getpost/checkLogin'
+import deleteItem from '../../getpost/deleteItem'
 
 import './FeedItem.scss'
 var dateFormat = require('dateformat');
@@ -11,35 +12,44 @@ interface Props {
     date: string;
     hash: string;
     whom: string;
+    wasadmin: string;
 }
 
-const FeedItem: React.FC<Props> = ({title, content, date, hash, whom}) => {
-    if (whom === "admin") whom = "admin (jeffrey)";
+const FeedItem: React.FC<Props> = ({title, content, date, hash, whom, wasadmin}) => {
+    const [d, setD] = useState<boolean>(false);
+
+    let fixedWhom = whom;
+    if (wasadmin === "1") fixedWhom += " (admin)";
 
     const isadmin: boolean = checkAdmin();
 
     const dateObj = new Date(date);
     const time = dateFormat(dateObj, "m/dd/yy");
 
-    const handleClick = () => {
-
+    const handleClick = async () => {
+        const res = await deleteItem(title, content, hash, whom);
+        if (res){
+            setD(false);
+        }
     }
 
     return (
-        <div className='FeedItem' data-date={date} data-hash={hash}>
-            <p className="whom">{whom} posted on {time}</p>
-            <p className='title'>{title}</p>
-            <p className='content'>{content}</p>
-            
-            <If cond={isadmin}>
-                <div className="trash" onClick={handleClick}>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" fill="currentColor" className="bi bi-trash" viewBox="0 0 16 16">
-                        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
-                        <path fillRule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
-                    </svg>
-                </div>
-            </If>
-        </div>
+        <If cond={true}>
+            <div className='FeedItem' data-date={date} data-hash={hash}>
+                <p className="whom">{fixedWhom} posted on {time}</p>
+                <p className='title'>{title}</p>
+                <p className='content'>{content}</p>
+                
+                <If cond={isadmin}>
+                    <div className="trash" onClick={handleClick}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" fill="currentColor" className="bi bi-trash" viewBox="0 0 16 16">
+                            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+                            <path fillRule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+                        </svg>
+                    </div>
+                </If>
+            </div>
+        </If>
     )
 }
 
